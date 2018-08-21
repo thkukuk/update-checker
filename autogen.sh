3b2d41a687123f19b866eadba0237f7016ba626a
Thorsten Kukuk <kukuk@thkukuk.de>
2018-08-21 15:32:05 +0200

    More configure/Makefile fixes

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
-rm -fv ltmain.sh config.sub config.guess config.h.in
-aclocal
+rm -fv aclocal.m4 ltmain.sh config.sub config.guess config.h.in
+aclocal -I m4
 automake --add-missing --copy --force
 autoreconf
 chmod 755 configure
